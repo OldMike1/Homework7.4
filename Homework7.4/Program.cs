c79ee609ad6b28f67fc5635894d6862ab4921335
mike19952006 <mike19952006@gmail.com>
2018-03-03 00:31:04 +0200

    Массив студентов, их рост, средний рост.

--- Homework7.4/Program.cs
+++ Homework7.4/Program.cs
@@ -10,6 +10,22 @@ namespace Homework7._4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Number of students");
+            int number = int.Parse(Console.ReadLine());
+            int[] students = new int[number];
+            Random rnd = new Random();
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = rnd.Next(130, 220);
+                Console.WriteLine($"student{i+1} = {students[i]}");
+            }
+            int sum = 0;
+            for (int i = 0; i < students.Length; i++)
+            {
+                sum +=  students[i];
+            }
+            int avarage = sum/number;
+            Console.WriteLine($"Average height = {avarage}");
         }
     }
 }
